a51faa3924a4c6523b65d1da7b9e7b12a24ec3a6
jtig37 <urldao@protonmail.com>
2024-11-09 09:37:48 -0800

    Update deploy-commands.js

--- commands/deploy-commands.js
+++ commands/deploy-commands.js
@@ -6,69 +6,68 @@ const { clientId, guildId, token } = require('../config.json');
 const commands = [
   new SlashCommandBuilder()
     .setName('register')
-    .setDescription('registers ethereum address')
+    .setDescription('Registers a Solana address')
     .setDefaultPermission(false)
     .addStringOption((option) =>
       option
         .setName('address')
-        .setDescription('Ethereum address starting in 0x...')
+        .setDescription('Solana address (44-character base58 format)')
         .setRequired(true)
     ),
   new SlashCommandBuilder()
     .setName('proposal')
-    .setDescription('creates a voting event')
+    .setDescription('Creates a voting event')
     .setDefaultPermission(false)
     .addStringOption((option) =>
-      option.setName('title').setDescription('proposal title').setRequired(true)
+      option.setName('title').setDescription('Proposal title').setRequired(true)
     )
     .addStringOption((option) =>
       option
         .setName('description')
-        .setDescription('description of proposal (max 4000 char.)')
+        .setDescription('Description of proposal (max 4000 characters)')
         .setRequired(true)
     )
     .addStringOption((option) =>
       option
         .setName('reactions')
         .setDescription(
-          'List reactions in the order that will be displayed, seperated by commas. (e.g. :one:,:two:,:three:)'
+          'List reactions in the order they will be displayed, separated by commas. (e.g. :one:,:two:,:three:)'
         )
         .setRequired(true)
     ),
   new SlashCommandBuilder()
     .setName('unregister')
     .setDescription(
-      'unregisters user or address (either option will properly remove from db)'
+      'Unregisters user or Solana address (either option will properly remove from database)'
     )
     .setDefaultPermission(false)
     .addStringOption((option) =>
       option
-        // There is a error with zod lib here where regex won't accept "userId" as the name
         .setName('user')
-        .setDescription('input users id to unregister')
+        .setDescription('Input user ID to unregister')
         .setRequired(false)
     )
     .addStringOption((option) =>
       option
         .setName('address')
-        .setDescription('input address to unregister')
+        .setDescription('Input Solana address to unregister')
         .setRequired(false)
     ),
   new SlashCommandBuilder()
     .setName('get-address')
-    .setDescription('gets address by user id')
+    .setDescription('Gets address by user ID')
     .setDefaultPermission(false)
     .addStringOption((option) =>
-      option.setName('user').setDescription('input users id').setRequired(true)
+      option.setName('user').setDescription('Input user ID').setRequired(true)
     ),
   new SlashCommandBuilder()
     .setName('get-user')
-    .setDescription('gets user id by address')
+    .setDescription('Gets user ID by Solana address')
     .setDefaultPermission(false)
     .addStringOption((option) =>
       option
         .setName('address')
-        .setDescription('input address')
+        .setDescription('Input Solana address')
         .setRequired(true)
     ),
 ].map((command) => command.toJSON());
